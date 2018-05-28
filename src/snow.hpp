2fdabf3963c0502bd37eaf09c0f6acb39814559f
r6eve <r6eve179f@gmail.com>
2018-05-28 14:36:20 +0900

    use inline

--- src/snow.hpp
+++ src/snow.hpp
@@ -16,11 +16,53 @@ class Snow {
 
   Snow_data snows_[snow_count];
 
-public:
+ public:
   Snow() {}
-  void init();
-  void update();
-  void draw(SDL_Surface *screen, ImageManager &image_manager);
+
+  inline void init() noexcept {
+    for (auto &snow : snows_) {
+      snow.view = false;
+    }
+  }
+
+  inline void update() noexcept {
+    for (auto &snow : snows_) {
+      if (snow.view) {
+        continue;
+      }
+      snow.view = true;
+      snow.x = rand() % (screen::width - 8);
+      snow.y = -8;
+      snow.type = rand() % 2;
+      break;
+    }
+
+    for (auto &snow : snows_) {
+      if (!snow.view) {
+        continue;
+      }
+      int move_speed[2] = {12, 8};
+      snow.y += move_speed[snow.type];
+      if (snow.y >= screen::height) {
+        snow.view = false;
+      }
+    }
+  }
+
+  inline void draw(SDL_Surface *screen, ImageManager &image_manager) noexcept {
+    for (auto &snow : snows_) {
+      if (!snow.view) {
+        continue;
+      }
+      SDL_Surface *p_surface = image_manager.get(image::snow);
+      SDL_Rect src = {static_cast<Sint16>(8 * snow.type), 0, 8,
+                      8};  // TODO: 8 is a size of snow.
+      SDL_Rect dst = {static_cast<Sint16>(snow.x), static_cast<Sint16>(snow.y),
+                      8, 8};
+      SDL_BlitSurface(p_surface, &src, screen, &dst);
+    }
+  }
+
   ~Snow() {}
 };
 
