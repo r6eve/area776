237be90f31b66513989caff459e6ea3a2f55e741
r6eve <r6eve179f@gmail.com>
2018-05-28 17:09:13 +0900

    easy refactoring

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,85 +1,90 @@
 #include <SDL/SDL_mixer.h>
 #include "def_global.hpp"
 #include "image_manager.hpp"
-#include "util.hpp"
 #include "mixer_manager.hpp"
+#include "util.hpp"
 
 void init_enemy() {
-  for (int i = 0; i < ENEMY_MAX; ++i) {
-    Enemy[i].view = false;
+  for (auto &enemy : Enemy) {
+    enemy.view = false;
   }
-  for (int i = 0; i < ENEMY_SHOT_MAX; ++i) {
-    Enemy_shot[i].view = false;
+  for (auto &shot : Enemy_shot) {
+    shot.view = false;
   }
 }
 
 void appear_enemy() {
-  double speed = 8;
-  int r = rand() % 45;
+  const double speed = 8;
+  if (rand() % 45 != 0) {
+    return;
+  }
 
-  if (r == 0) {
-    for (int i = 0; i < ENEMY_MAX; ++i) {
-      if (Enemy[i].view) {
-        continue;
-      }
-      Enemy[i].view = true;
-      Enemy[i].pos.x = rand() % (screen::width - 64);
-      Enemy[i].pos.y = -64;
-      Enemy[i].shot_timer = rand() % 15 + 15;
-      sub_vec(Enemy[i].move, Fighter.pos, Enemy[i].pos);
-      normalize_vec(Enemy[i].move);
-      mul_vec(Enemy[i].move, speed);
-      break;
+  for (auto &enemy : Enemy) {
+    if (enemy.view) {
+      continue;
     }
+    enemy.view = true;
+    enemy.pos.x = rand() % (screen::width - 64);
+    enemy.pos.y = -64;
+    enemy.shot_timer = rand() % 15 + 15;
+    sub_vec(enemy.move, Fighter.pos, enemy.pos);
+    normalize_vec(enemy.move);
+    mul_vec(enemy.move, speed);
+    break;
   }
 }
 
 void move_enemy(MixerManager &mixer_manager) {
-  double speed = 6;
-  double shot_pitch = 20;
+  const double speed = 6;
+  const double shot_pitch = 20;
 
-  for (int i = 0; i < ENEMY_MAX; ++i) {
-    if (!Enemy[i].view) {
+  for (auto &enemy : Enemy) {
+    if (!enemy.view) {
       continue;
     }
-    add_vec(Enemy[i].pos, Enemy[i].move);
-    if (Enemy[i].pos.x < -35) {
-      Enemy[i].view = 0;
-    }
-    if (Enemy[i].pos.x > screen::width) {
-      Enemy[i].view = 0;
-    }
-    if (Enemy[i].pos.y > screen::height) {
-      Enemy[i].view = 0;
-    }
-    if (!Enemy[i].view) {
+
+    add_vec(enemy.pos, enemy.move);
+    if (enemy.pos.x < -35) {
+      enemy.view = false;
       continue;
     }
-    --Enemy[i].shot_timer;
-    if (Enemy[i].shot_timer == 0) {
-      Vector enemy_center = {Enemy[i].pos.x + 32, Enemy[i].pos.y + 32};
-      Vector fighter_center = {Fighter.pos.x + 32, Fighter.pos.y + 32};
-      Vector vec;
-      sub_vec(vec, fighter_center, enemy_center);
-      normalize_vec(vec);
-      mul_vec(vec, speed);
-      /* 時計回りに(shot_pitch * 2)度回転させておく */
-      double rot_angle = -(shot_pitch * 2) * M_PI / 180;
-      rot_vec(vec, rot_angle);
-      for (int j = 0; j < 5; ++j) {
-        for (int k = 0; k < ENEMY_SHOT_MAX; ++k) {
-          if (Enemy_shot[k].view) {
-            continue;
-          }
-          Enemy_shot[k].view = true;
-          cp_vec(Enemy_shot[k].pos, enemy_center);
-          cp_vec(Enemy_shot[k].move, vec);
-          break;
+    if (enemy.pos.x > screen::width) {
+      enemy.view = false;
+      continue;
+    }
+    if (enemy.pos.y > screen::height) {
+      enemy.view = false;
+      continue;
+    }
+
+    --enemy.shot_timer;
+    if (enemy.shot_timer != 0) {
+      continue;
+    }
+
+    Vector enemy_center = {enemy.pos.x + 32, enemy.pos.y + 32};
+    Vector fighter_center = {Fighter.pos.x + 32, Fighter.pos.y + 32};
+    Vector vec;
+    sub_vec(vec, fighter_center, enemy_center);
+    normalize_vec(vec);
+    mul_vec(vec, speed);
+    /* 時計回りに(shot_pitch * 2)度回転させておく */
+    const double rot_angle = -(shot_pitch * 2) * M_PI / 180;
+    rot_vec(vec, rot_angle);
+    for (int _ = 0; _ < 5; ++_) {
+      for (auto &shot : Enemy_shot) {
+        if (shot.view) {
+          continue;
         }
-        double rot_angle = shot_pitch * M_PI / 180;
-        rot_vec(vec, rot_angle);
-        Mix_PlayChannel(-1, mixer_manager.get_se(se_type::enemy_shoot), 0);
+
+        shot.view = true;
+        cp_vec(shot.pos, enemy_center);
+        cp_vec(shot.move, vec);
+        break;
       }
+      const double rot_angle = shot_pitch * M_PI / 180;
+      rot_vec(vec, rot_angle);
+      Mix_PlayChannel(-1, mixer_manager.get_se(se_type::enemy_shoot), 0);
     }
   }
 }
@@ -106,33 +111,42 @@ void move_enemy_shot() {
 }
 
 bool check_myshots_hit_enemy() {
-  for (int i = 0; i < ENEMY_MAX; ++i) {
-    if (!Enemy[i].view) continue;
-    for (int j = 0; j < FIGHTER_SHOT_MAX; ++j) {
-      if (!Fighter_shot[j].view) {
+  for (auto &enemy : Enemy) {
+    if (!enemy.view) {
+      continue;
+    }
+
+    for (auto &shot : Fighter_shot) {
+      if (!shot.view) {
         continue;
       }
-      SDL_Rect r1 = {Enemy[i].pos.x, Enemy[i].pos.y, 35, 35};
-      SDL_Rect r2 = {Fighter_shot[j].pos.x, Fighter_shot[j].pos.y, 10, 24};
+
+      SDL_Rect r1 = {static_cast<Sint16>(enemy.pos.x),
+                     static_cast<Sint16>(enemy.pos.y), 35, 35};
+      SDL_Rect r2 = {static_cast<Sint16>(shot.pos.x),
+                     static_cast<Sint16>(shot.pos.y), 10, 24};
       if (!check_hit_rect(&r1, &r2)) {
         continue;
       }
+
       ++Enemy_life;
-      Enemy[i].view = false;
-      Fighter_shot[j].view = false;
-      for (int k = 0; k < EFFECT_MAX; ++k) {
-        if (Effect[k].view) {
+      enemy.view = false;
+      shot.view = false;
+      for (auto &effect : Effect) {
+        if (effect.view) {
           continue;
         }
-        Effect[k].view = true;
-        Effect[k].pos.x = -80 + Enemy[i].pos.x + r1.w / 2;
-        Effect[k].pos.y = -80 + Enemy[i].pos.y + r1.h / 2;
-        Effect[k].count = 0;
+
+        effect.view = true;
+        effect.pos.x = -80 + enemy.pos.x + r1.w / 2;
+        effect.pos.y = -80 + enemy.pos.y + r1.h / 2;
+        effect.count = 0;
         break;
       }
       if (Enemy_life > 29) {
         return true;
       }
+
       break;
     }
   }
@@ -140,23 +154,27 @@ bool check_myshots_hit_enemy() {
 }
 
 void draw_enemy(SDL_Surface *screen, ImageManager &image_manager) {
-  for (int i = 0; i < ENEMY_MAX; ++i) {
-    if (!Enemy[i].view) {
+  for (auto &enemy : Enemy) {
+    if (!enemy.view) {
       continue;
     }
+
     SDL_Surface *p_surface = image_manager.get(image::mons13);
-    SDL_Rect dst = {Enemy[i].pos.x, Enemy[i].pos.y};
-    SDL_BlitSurface(p_surface, NULL, screen, &dst);
+    SDL_Rect dst = {static_cast<Sint16>(enemy.pos.x),
+                    static_cast<Sint16>(enemy.pos.y), 35, 35};
+    SDL_BlitSurface(p_surface, nullptr, screen, &dst);
   }
 }
 
 void draw_enemy_shot(SDL_Surface *screen, ImageManager &image_manager) {
-  for (int i = 0; i < ENEMY_SHOT_MAX; ++i) {
-    if (!Enemy_shot[i].view) {
+  for (auto &shot : Enemy_shot) {
+    if (!shot.view) {
       continue;
     }
+
     SDL_Surface *p_surface = image_manager.get(image::bm01);
-    SDL_Rect dst = {Enemy_shot[i].pos.x, Enemy_shot[i].pos.y};
-    SDL_BlitSurface(p_surface, NULL, screen, &dst);
+    SDL_Rect dst = {static_cast<Sint16>(shot.pos.x),
+                    static_cast<Sint16>(shot.pos.y), 16, 16};
+    SDL_BlitSurface(p_surface, nullptr, screen, &dst);
   }
 }
