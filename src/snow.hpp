eebf400ade09afe63f222a0ac74fcdc8850f9ca4
r6eve <r6eve179f@gmail.com>
2018-06-01 18:14:31 +0900

    use the value of SDL_Surface

--- src/snow.hpp
+++ src/snow.hpp
@@ -8,7 +8,7 @@ const int snow_count = 256;
 
 class Snow {
   struct Snow_data {
-    bool view;
+    bool view_p;
     int x;
     int y;
     int type;
@@ -21,16 +21,16 @@ class Snow {
 
   inline void init() noexcept {
     for (auto &snow : snows_) {
-      snow.view = false;
+      snow.view_p = false;
     }
   }
 
   inline void update() noexcept {
     for (auto &snow : snows_) {
-      if (snow.view) {
+      if (snow.view_p) {
         continue;
       }
-      snow.view = true;
+      snow.view_p = true;
       snow.x = rand() % (screen::width - 8);
       snow.y = -8;
       snow.type = rand() % 2;
@@ -38,13 +38,13 @@ class Snow {
     }
 
     for (auto &snow : snows_) {
-      if (!snow.view) {
+      if (!snow.view_p) {
         continue;
       }
       int move_speed[2] = {12, 8};
       snow.y += move_speed[snow.type];
       if (snow.y >= screen::height) {
-        snow.view = false;
+        snow.view_p = false;
       }
     }
   }
@@ -52,13 +52,15 @@ class Snow {
   inline void draw(SDL_Surface *screen, const ImageManager &image_manager) const
       noexcept {
     for (const auto &snow : snows_) {
-      if (!snow.view) {
+      if (!snow.view_p) {
         continue;
       }
       SDL_Surface *p_surface = image_manager.get(image::snow);
-      SDL_Rect src = {static_cast<Sint16>(8 * snow.type), 0, 8, 8};
+      const Uint16 w = static_cast<Uint16>(p_surface->w / 2);
+      SDL_Rect src = {static_cast<Sint16>(w * snow.type), 0, w,
+                      static_cast<Uint16>(p_surface->h)};
       SDL_Rect dst = {static_cast<Sint16>(snow.x), static_cast<Sint16>(snow.y),
-                      8, 8};
+                      w, static_cast<Uint16>(p_surface->h)};
       SDL_BlitSurface(p_surface, &src, screen, &dst);
     }
   }
